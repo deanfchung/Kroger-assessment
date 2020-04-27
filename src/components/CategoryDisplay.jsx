ccc37c4ac41d424ae88962cb6150e83cea4935c6
dean <deanthechung@gmail.com>
2020-04-27 14:46:35 -0700

    colors

--- src/components/CategoryDisplay.jsx
+++ src/components/CategoryDisplay.jsx
@@ -4,9 +4,9 @@ import styled from 'styled-components'
 const CategoryDisplay = (props) => {
     const { activeCategory, menu } = props;
 
-    const tableItems = menu ? menu.reduce((array, item) => {
+    const tableItems = menu ? menu.reduce((array, item, index) => {
         array.push(
-            <tr key={item.id}>
+            <tr key={item.id} className={index % 2 === 0 ? 'even' : 'odd'}>
                 <td>{item.name}</td>
                 <td>{item.description}</td>
             </tr>)
@@ -34,10 +34,12 @@ const CategoryDisplay = (props) => {
 const Table = styled.table`
     border-collapse: collapse;  
     width: 75%;
-
     td {
         border: 1px solid black;
         font-size: 15px
+    }
+    .even {
+        background-color: #cecacf;
     }
 `
 export default CategoryDisplay;
